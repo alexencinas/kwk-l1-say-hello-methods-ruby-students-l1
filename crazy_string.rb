b27155e89e7484fc6e608e4d8080131606507ad5
alexencinas <shoogiee123@gmail.com>
2018-06-05 17:34:33 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,3 +1,5 @@
-def crazy_strings
-  puts "Hello Lydia".reverse.
-  puts"Hello Emma
+def crazy_strings (string1, string2)
+  puts "Hello #{string1}".reverse.upase
+  puts"Hello #{string2}".swapcase.gsub(/s/, "z")
+end("Lydia", "Taylor")
+crazy_strings
